bf1e198abe3dcaa3c7350dcc50527525fecaea78
SaurabhMulay999 <90036775+SaurabhMulay999@users.noreply.github.com>
2023-09-03 23:43:36 +0530

    Browse Route

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,29 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from './Utils/firebase'
+import { useNavigate } from 'react-router-dom'
 
-function Header() {
+function Header(prop) {
+  const navigate = useNavigate();
+
+  function HandleSignOut() {
+    signOut(auth).then(() => {
+      navigate('/');
+    }).catch(() => {
+      navigate('/Error')
+    })
+  }
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black w-full h-46 z-10">
+    <div className="flex justify-between absolute px-8 py-2 bg-gradient-to-b from-black w-full h-46 z-10">
           <img src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=700&h=456" alt='Netflix' className='w-44' />
-         
+      <div className='flex mt-6 p-2'>
+        <img  hidden={prop.signIn } className="h-9 w-9 " src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="logo" />
+        <button  hidden={prop.signIn} onClick={HandleSignOut} className='p-1 m-1 text-white font-bold'>Sign out</button>
+      </div>
+
     </div>
   )
 }
 
-export default Header
+  export default Header;

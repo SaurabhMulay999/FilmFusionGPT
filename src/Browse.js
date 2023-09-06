36756942099875443ba0cfd099436f728f2b593f
SaurabhMulay999 <90036775+SaurabhMulay999@users.noreply.github.com>
2023-09-06 21:59:27 +0530

    FixedCustomHooks

--- src/Browse.js
+++ src/Browse.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import { options } from './constants/constant'
+import useNowPlay from './hooks/useNowPlay'
 
 const Browse = () => {
-  
+  //calling custom Hook
+  useNowPlay();
      
   return (
     <div>
--- src/hooks/useNowPlay.js
+++ src/hooks/useNowPlay.js
@@ -2,17 +2,22 @@ import { useEffect } from "react";
 import { addNowPlayMovies } from '../Utils/movieSlice.js';
 import { options } from "../constants/constant.jsx";
 import { useDispatch } from "react-redux";
-useDispatch
-function useNowPlay() {
+
+
+const useNowPlay=function() {
+    const dispatch = useDispatch();
+
+    const getNowPlayMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
+        const json = await data.json();
+        console.log(json);
+        dispatch(addNowPlayMovies(json.results));
+      }
+
     useEffect(function () {
-        const dispatch = useDispatch();
         getNowPlayMovies();
-        const getNowPlayMovies = async () => {
-            const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
-            const json = await data.json();
-            console.log(json);
-            dispatch(addNowPlayMovies(json.results));
-          }
     }, [])
   
 }
+
+export default useNowPlay;
